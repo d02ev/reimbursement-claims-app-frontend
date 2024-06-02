0361b158654efb3d93e760b71d17716fe428b71a
d02ev <vikramadityapratapsingh98@gmail.com>
2024-06-02 14:22:16 +0530

    updated backend API url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	apiUrl: 'https://reimbursement-claims-app-backend.onrender.com/api',
+	apiUrl: 'https://reimbursementclaimsapp-d02ev-5423d850.koyeb.app/api',
 	production: true,
 };
